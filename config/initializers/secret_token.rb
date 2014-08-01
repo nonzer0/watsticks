af36381cee735aac0756898e611feb3b5c34d1bd
Neil West <nswest23@gmail.com>
2014-08-01 12:31:49 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watsticks::Application.config.secret_key_base = '83259bc8dafe13b1b6b34b7c947ac469a51d99a405bfde8a98f64b5a049d56c1e1e93474f8d48631e658890b42b4908792917fb00f263101b53bb3a46589a9d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Watsticks::Application.config.session_store :cookie_store, key: '_watsticks_session'
