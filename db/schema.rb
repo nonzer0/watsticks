c3c76526914e75f471d4d1c52cd4d7abcc3e4c8b
Neil West <nswest23@gmail.com>
2014-08-04 15:51:40 -0400

    adding pg in dev

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140804012651) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: true do |t|
     t.string   "title"
     t.string   "company"
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 20140804012651) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
