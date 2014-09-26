3ca2be3a97ca5097ff28f61b9f2e460fa8796eca
Neil West <nswest23@gmail.com>
2014-09-25 22:29:28 -0400

    finish has_many relationship of job and interviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925190902) do
+ActiveRecord::Schema.define(version: 20140926022522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140925190902) do
     t.time     "scheduled_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "job_id"
   end
 
   create_table "jobs", force: true do |t|
--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -4,7 +4,7 @@ describe Interview  do
   let(:user) { FactoryGirl.create(:user_with_jobs) }
   #let(:job) { FactoryGirl.create(:job) }
   #before { @interview = job.interviews.build() }
-  before { @interview = user.jobs.first.interview.build(scheduled_on: '2014-12-31',
+  before { @interview = user.jobs.first.interviews.build(scheduled_on: '2014-12-31',
   														scheduled_time: '12:30 pm' ) }
   subject { @interview }
 
