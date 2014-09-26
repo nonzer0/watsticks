73eaec75857bcc1531cfaa3ed01eacf371ef9654
Neil West <westn2@upmc.edu>
2014-09-26 16:12:45 -0400

    changed scheduled_on to datetime and removed scheduled_time from interviews model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926022522) do
+ActiveRecord::Schema.define(version: 20140926201022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "interviews", force: true do |t|
-    t.date     "scheduled_on"
-    t.time     "scheduled_time"
+    t.datetime "scheduled_on"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "job_id"
