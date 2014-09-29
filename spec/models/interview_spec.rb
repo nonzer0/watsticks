0c5aaea258f4f40d539c18065394b9c05c87d6be
Neil West <westn2@upmc.edu>
2014-09-29 08:02:53 -0400

    fix tests

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 describe Interview  do
-  let(:user) { FactoryGirl.create(:user_with_jobs) }
-  #let(:job) { FactoryGirl.create(:job) }
-  #before { @interview = job.interviews.build() }
-  before { @interview = user.jobs.first.interviews.build(scheduled_on: '2014-12-31',
-  														scheduled_time: '12:30 pm' ) }
+  let(:job) { FactoryGirl.create(:job) }
+  before { @interview = job.interviews.build(scheduled_on: '2014-12-31') }
   subject { @interview }
 
   it { should respond_to(:scheduled_on) }
-  it { should respond_to(:scheduled_time) }
+
+  it { should be_valid }
 end
