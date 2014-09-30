254c18cae699103b01422c67516eff5d0e88bc9a
Neil West <nswest23@gmail.com>
2014-09-29 21:55:56 -0400

    move interview controller spec to right place

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,16 @@ ActiveRecord::Schema.define(version: 20140929143053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "position"
+    t.string   "phone_number"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "job_id"
+  end
+
   create_table "interviews", force: true do |t|
     t.datetime "scheduled_on"
     t.datetime "created_at"
